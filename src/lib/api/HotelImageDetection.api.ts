b07fb9b0c1d159c7315d6d9a2ed4554ddaa733b8
Leon Gauweiler <leon.gauweiler@gmail.com>
2024-07-27 17:13:33 +0200

    added: api

---
+++ src/lib/api/HotelImageDetection.api.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+class HotelImageDetectionAPI {
+  private apiUrl: string;
+
+  constructor() {
+    if (process.env.API_URL === undefined) {
+        throw new Error('No API Url defined');
+    }
+
+    this.apiUrl = process.env.API_URL;
+  }
+
+  async detectImage(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const response = await axios.post(`${this.apiUrl}/hotel-image-detection/predict`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error in image detection:', error);
+      throw error;
+    }
+  }
+}
+
+export default new HotelImageDetectionAPI();
